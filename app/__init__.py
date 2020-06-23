d93b3e1f1fd80a44f549a2a801335488db57681f
yao <yao.ntno@gmail.com>
2020-06-23 15:25:27 +0000

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from logging import getLogger, StreamHandler, Formatter, INFO, FileHandler
 from pathlib import Path
+from datetime import datetime
 from . import config
 
 logger = getLogger()
@@ -9,8 +10,7 @@ stream_handler.setLevel(INFO)
 handler_format = Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 stream_handler.setFormatter(handler_format)
 logger.addHandler(stream_handler)
-
-file_handler = FileHandler(filename="app.log")
+file_handler = FileHandler(filename=datetime.now().strftime("%Y%m%d-%H%M%S") + ".log")
 logger.addHandler(file_handler)
 file_handler.setLevel(INFO)
 file_handler.setFormatter(handler_format)
