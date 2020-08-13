6ab888a5d29156d24fb29810cf8566862f558e90
xinyuan-yao <yao.ntno@gmail.com>
2020-08-13 12:13:45 +0000

    add test

--- tests/models/test_losses.py
+++ tests/models/test_losses.py
@@ -8,12 +8,14 @@ from torch import nn, Tensor
 def test_diou() -> None:
     fn = DIoU()
     pred_boxes = PascalBoxes(
-        torch.tensor([[0.1, 0.1, 0.2, 0.2], [0.2, 0.2, 0.3, 0.3],])
+        torch.tensor(
+            [[0.0, 0.0, 0.1, 0.1], [0.1, 0.1, 0.2, 0.2], [0.2, 0.2, 0.3, 0.3],]
+        )
     )
     tgt_boxes = PascalBoxes(torch.tensor([[0.2, 0.2, 0.3, 0.3]]))
     res = fn(pred_boxes, tgt_boxes,)
-    assert res.shape == (2, 1)
-    assert F.l1_loss(res, torch.tensor([[1.25], [0.0]])) < 1e-7
+    assert res.shape == (3, 1)
+    assert F.l1_loss(res, torch.tensor([[1.444444], [1.25], [0.0]])) < 1e-6
 
 
 def test_giou() -> None:
