b6dbe837b29106ae2edbf093b45fb41282675b9f
h4nyu <yao.ntno@gmail.com>
2020-08-13 06:48:23 +0000

    add giou

--- object_detection/models/losses.py
+++ object_detection/models/losses.py
@@ -3,6 +3,8 @@ import numpy as np
 import torch.nn.functional as F
 from typing_extensions import Literal
 from torch import Tensor
+from object_detection.entities import PascalBoxes
+from torchvision.ops.boxes import box_area
 import torch
 import torch.nn as nn
 
@@ -35,3 +37,40 @@ class FocalLoss(nn.Module):
         neg_loss = -(pred ** gamma) * torch.log(1 - pred)
         loss = pos_loss + neg_loss
         return loss
+
+
+class IoU:
+    def __call__(
+        self, boxes1: PascalBoxes, boxes2: PascalBoxes
+    ) -> t.Tuple[Tensor, Tensor]:
+        area1 = box_area(boxes1)
+        area2 = box_area(boxes2)
+        lt = torch.max(boxes1[:, None, :2], boxes2[:, :2])  # [N,M,2]
+        rb = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])  # [N,M,2]
+
+        wh = (rb - lt).clamp(min=0)  # [N,M,2]
+        inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
+
+        union = area1[:, None] + area2 - inter
+
+        iou = inter / union
+        return iou, union
+
+
+class GIoU:
+    def __init__(self) -> None:
+        self.box_iou = IoU()
+
+    def __call__(self, src: PascalBoxes, tgt: PascalBoxes) -> Tensor:
+        iou, union = self.box_iou(src, tgt)
+        lt = torch.min(src[:, None, :2], tgt[:, :2])
+        rb = torch.max(src[:, None, 2:], tgt[:, 2:])  # [N,M,2]
+        wh = (rb - lt).clamp(min=0)  # [N,M,2]
+        area = wh[:, :, 0] * wh[:, :, 1]
+        return iou - (area - union) / area
+
+
+class BIoU:
+    def __call__(self, src: PascalBoxes, tgt: PascalBoxes) -> None:
+
+        print(src)
