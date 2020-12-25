90168d3fdc40f9d0319d57b39ec202805fa83b7b
xinyuan-yao <yao.ntno@gmail.com>
2020-12-25 23:40:10 +0000

    fix ?

--- object_detection/models/anchors.py
+++ object_detection/models/anchors.py
@@ -70,7 +70,7 @@ class Anchors:
         grid_x1y1 = grid_x0y0 + box_wh
         boxes = (
             torch.cat([grid_x0y0, grid_x1y1], dim=1)
-            .permute(3, 2, 0, 1)
+            .permute(2, 3, 0, 1)
             .contiguous()
             .view(-1, 4)
         )
