9fef675c122064ea5d1ffaea70c7f9170e8a0805
xinyuan.yao <yao.ntno@gmail.com>
2021-01-15 20:28:44 +0900

    fix anchor

--- tests/models/test_anchors.py
+++ tests/models/test_anchors.py
@@ -18,8 +18,8 @@ from object_detection.utils import DetectionPlot
     "scales, ratios, size",
     [
         (
-            [3/2, 2/3],
-            [1.0, 1.25],
+            [1.0],
+            [1.0],
             1,
         ),
         (
@@ -39,20 +39,20 @@ def test_anchors(
     ratios: List[float],
     size: int,
 ) -> None:
-    base_size = 1024 + 512
+    original_w = 1024 + 512
+    original_h = 1024
     stride = 2 ** 7
-    h = base_size // stride
-    w = base_size // stride
+    h = original_h // stride
+    w = original_w // stride
     images = ImageBatch(torch.zeros((1, 3, h, w), dtype=torch.float32))
     fn = Anchors(size=size, scales=scales, ratios=ratios)
     res = fn(images, stride)
     num_anchors = len(scales) * len(ratios)
     anchor_count = w * h * num_anchors
     assert res.shape == (anchor_count, 4)
-    assert base_size == res.max()
     assert 0 == res.min()
-    plot = DetectionPlot(torch.ones((3, base_size, base_size)))
-    plot.draw_boxes(boxes=res[0:num_anchors], color="red")
+    plot = DetectionPlot(torch.ones((3, original_h, original_w)))
+    plot.draw_boxes(boxes=res[0:4], color="red")
     plot.draw_boxes(boxes=res[-4:], color="blue")
     plot.save(
         f"store/test-anchors-{size}-{stride}-{'-'.join([str(x) for x in  scales])}-{num_anchors}.png"
