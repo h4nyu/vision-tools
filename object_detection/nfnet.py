aad35eea1a70d2567db84575b09f366e66fb7929
xinyuan-yao <yao.ntno@gmail.com>
2021-02-24 14:10:51 +0000

    add nfblock

--- object_detection/nfnet.py
+++ object_detection/nfnet.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn, Tensor
 from torch.functional import F
+from typing import Callable, Optional
 
 
 class WSConv2d(nn.Conv2d):
@@ -9,13 +10,13 @@ class WSConv2d(nn.Conv2d):
         in_channels: int,
         out_channels: int,
         kernel_size: int,
-        stride:int=1,
-        padding:int=0,
-        dilation:int=1,
-        groups:int=1,
-        bias:bool=True,
-        padding_mode:str="zeros",
-        eps:float=1e-4,
+        stride: int = 1,
+        padding: int = 0,
+        dilation: int = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = "zeros",
+        eps: float = 1e-4,
     ):
         super().__init__(
             in_channels,
@@ -40,10 +41,9 @@ class WSConv2d(nn.Conv2d):
             torch.max(var * fan_in, torch.tensor(self.eps).to(var.device))
         ) * self.gain.view_as(var).to(var.device)
         shift = mean * scale
-        print(shift)
         return self.weight * scale - shift
 
-    def forward(self, input:Tensor) -> Tensor:
+    def forward(self, input: Tensor) -> Tensor:
         weight = self.standardize_weight()
         return F.conv2d(
             input,
@@ -54,3 +54,108 @@ class WSConv2d(nn.Conv2d):
             self.dilation,
             self.groups,
         )
+
+
+class SqueezeExcite(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        activation: Callable[[Tensor], Tensor],
+    ) -> None:
+        super().__init__()
+        self.se_conv0 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=True)
+        self.se_conv1 = nn.Conv2d(out_channels, in_channels, kernel_size=1, bias=True)
+        self.activation = activation
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = torch.mean(x, dim=[2, 3], keepdim=True)
+        out = self.se_conv1(self.activation(self.se_conv0(out)))
+        return (torch.sigmoid(out) * 2) * x
+
+
+class NFBlock(nn.Module):
+    conv_shortcut: Optional[WSConv2d]
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        stride: int = 1,
+        beta: float = 1.0,
+        alpha: float = 1.0,
+        expansion: float = 2.25,
+        se_ratio: float = 0.5,
+        group_size: Optional[int] = 8,
+        activation: Callable[[Tensor], Tensor] = F.relu,
+    ) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.beta = beta
+        self.alpha = alpha
+        self.expansion = expansion
+        self.se_ratio = se_ratio
+        self.group_size = group_size
+        self.activation = activation
+
+        hidden_channels = int(expansion * in_channels)
+        if group_size is None:
+            groups = 1
+        else:
+            groups = hidden_channels // group_size
+            hidden_channels = int(group_size * groups)
+
+        self.hidden_channels = hidden_channels
+
+        self.conv1x1a = WSConv2d(
+            in_channels=in_channels,
+            out_channels=hidden_channels,
+            kernel_size=1,
+            padding=0,
+        )
+        self.conv3x3 = WSConv2d(
+            in_channels=hidden_channels,
+            out_channels=hidden_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            groups=groups,
+        )
+        self.conv1x1b = WSConv2d(
+            in_channels=hidden_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            padding=0,
+        )
+        if in_channels != out_channels:
+            self.conv_shortcut = WSConv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=1,
+                padding=0,
+            )
+        else:
+            self.conv_shortcut = None
+
+        se_channels = max(1, int(hidden_channels * se_ratio))
+        self.se = SqueezeExcite(
+            in_channels=hidden_channels,
+            out_channels=out_channels,
+            activation=activation,
+        )
+        self.skip_init_gain = nn.Parameter(torch.zeros(()))
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.activation(x) / self.beta
+        shortcut = x
+        if self.stride > 1:
+            shortcut = F.avg_pool2d(shortcut, 2)
+        if self.conv_shortcut is not None:
+            shortcut = self.conv_shortcut(shortcut)
+        out = self.conv1x1a(out)
+        out = self.conv3x3(self.activation(out))
+        out = self.se(out)
+        out = self.conv1x1b(self.activation(out))
+        return out * self.skip_init_gain * self.alpha + shortcut
--- tests/test_nfnet.py
+++ tests/test_nfnet.py
@@ -1,5 +1,6 @@
 import torch
-from object_detection.nfnet import WSConv2d
+from object_detection.nfnet import WSConv2d, NFBlock
+
 
 def test_wsconv2d() -> None:
     in_channels = 32
@@ -14,3 +15,61 @@ def test_wsconv2d() -> None:
     y = block(x)
     # TODO assert
 
+
+def test_nfblock_same_size() -> None:
+    channels = 64
+    h, w = 128, 128
+    block = NFBlock(
+        in_channels=channels,
+        out_channels=channels,
+    )
+    x = torch.ones(1, channels, h, w)
+    out = block(x)
+    assert x.shape == out.shape
+
+
+def test_nfblock_downsize() -> None:
+    channels = 64
+    h, w = 128, 128
+    block = NFBlock(
+        in_channels=channels,
+        out_channels=channels,
+        stride=2,
+    )
+    x = torch.ones(1, channels, h, w)
+    out = block(x)
+    assert x.shape[:2] == out.shape[:2]
+    assert x.shape[2] // 2 == out.shape[2]
+    assert x.shape[3] // 2 == out.shape[3]
+
+
+def test_nfblock_different() -> None:
+    in_channels = 64
+    out_channels = 48
+    h, w = 128, 128
+    block = NFBlock(
+        in_channels=in_channels,
+        out_channels=out_channels,
+    )
+    x = torch.ones(1, in_channels, h, w)
+    out = block(x)
+    assert x.shape[2:] == out.shape[2:]
+    assert x.shape[0] == out.shape[0]
+    assert out.shape[1] == out_channels
+
+
+def test_nfblock_different_downsize() -> None:
+    in_channels = 64
+    out_channels = 48
+    h, w = 128, 128
+    block = NFBlock(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        stride=2,
+    )
+    x = torch.ones(1, in_channels, h, w)
+    out = block(x)
+    assert x.shape[0] == out.shape[0]
+    assert out.shape[1] == out_channels
+    assert x.shape[2] // 2 == out.shape[2]
+    assert x.shape[3] // 2 == out.shape[3]
