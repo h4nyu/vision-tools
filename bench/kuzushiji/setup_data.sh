b015030e33db2a2e93977c36da34454b0c4f00ec
h4nyu <yao.ntno@gmail.com>
2021-12-29 15:31:58 +0900

    remove

--- bench/kuzushiji/setup_data.sh
+++ bench/kuzushiji/setup_data.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
-
+kaggle competitions download -c kuzushiji-recognition -p /store/kuzushiji-recognition
+cd /store/kuzushiji-recognition
+unzip kuzushiji-recognition.zip
+unzip test_images.zip
+unzip train_images.zip
