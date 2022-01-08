5cb706e2be7d77f716c50423a66d488eeb4fc635
h4nyu <yao.ntno@gmail.com>
2022-01-09 01:19:51 +0900

    add backbone

--- vision_tools/vision_tools/backbone.py
+++ vision_tools/vision_tools/backbone.py
@@ -1,6 +1,36 @@
 from torch import nn, Tensor
 from typing import Callable
 from .block import DefaultActivation, DWConv, ConvBnAct, Focus, CSP, SPP
+from efficientnet_pytorch import EfficientNet as _EfficientNet
+
+
+efficientnet_channels = {
+    "efficientnet-b0": [3, 16, 24, 40, 112, 320, 1280],
+    "efficientnet-b1": [3, 16, 24, 40, 112, 320, 1280],
+    "efficientnet-b2": [3, 16, 24, 48, 120, 352, 1408],
+    "efficientnet-b3": [3, 24, 32, 48, 136, 384, 1536],
+    "efficientnet-b4": [3, 24, 32, 56, 160, 448, 1792],
+    "efficientnet-b5": [3, 24, 40, 64, 176, 512, 2048],
+    "efficientnet-b6": [3, 32, 40, 72, 200, 576, 2304],
+    "efficientnet-b7": [3, 32, 48, 80, 224, 640, 2560],
+}
+efficientnet_strides = [1, 2, 4, 8, 16, 32, 32]
+
+
+class EfficientNet(nn.Module):
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
+        super().__init__()
+        self.net = _EfficientNet.from_pretrained(name)
+        self.out_len = 6
+        self.channels = efficientnet_channels[name][: self.out_len]
+        self.strides = efficientnet_strides[: self.out_len]
+
+    def forward(self, images: Tensor) -> list[Tensor]:  # P1 - P6, P7 is dropped
+        features = self.net.extract_endpoints(images)
+        return [images, *features.values()][: self.out_len]
 
 
 class CSPDarknet(nn.Module):
