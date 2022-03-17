462031c318f242866d536de854a647583980688d
h4nyu <yao.ntno@gmail.com>
2022-03-17 14:05:54 +0900

    setup

---
+++ tl_sandbox/pipeline/dodo.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import Any, Callable
+import pickle
+
+
+class CacheAction:
+    def _persist(self, key: str, func: Callable) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            res = func(*args, **kwargs)
+            with open(key, "wb") as fp:
+                pickle.dump(res, fp)
+
+        return wrapper
+
+    def __call__(
+        self,
+        fn: Any,
+        args: list[Any] = [],
+        kwargs: dict[str, Any] = {},
+        output_args: list[str] = [],
+        output_kwargs: dict[str, str] = {},
+        key: str = None,
+    ) -> tuple[Any, list, dict]:
+        if key is not None:
+            fn = self._persist(key, fn)
+
+        def wrapper() -> Any:
+            _kwargs = {}
+            _args = []
+            for k, v in output_kwargs.items():
+                with open(v, "rb") as fp:
+                    _kwargs[k] = pickle.load(fp)
+            for v in output_args:
+                with open(v, "rb") as fp:
+                    _args.append(pickle.load(fp))
+            res = fn(*[*args, *_args], **{**kwargs, **_kwargs})
+
+        return (wrapper, [], {})
--- tl_sandbox/setup.py
+++ tl_sandbox/setup.py
@@ -3,6 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="tl_sandbox",
     version="0.1.0",
+    install_requires=[
+        "timm",
+        "pytorch-metric-learning",
+        "doit",
+        "pytorch-lightning"
+    ],
     packages=find_packages(),
     package_data={"tl_sandbox": ["py.typed"]},
 )
