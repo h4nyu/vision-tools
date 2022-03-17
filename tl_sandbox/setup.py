4417f534445185466bb9046abc0a95f19f140a22
h4nyu <yao.ntno@gmail.com>
2022-03-17 14:06:07 +0900

    setup

--- tl_sandbox/setup.py
+++ tl_sandbox/setup.py
@@ -3,12 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="tl_sandbox",
     version="0.1.0",
-    install_requires=[
-        "timm",
-        "pytorch-metric-learning",
-        "doit",
-        "pytorch-lightning"
-    ],
+    install_requires=["timm", "pytorch-metric-learning", "doit", "pytorch-lightning"],
     packages=find_packages(),
     package_data={"tl_sandbox": ["py.typed"]},
 )
