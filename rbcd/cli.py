b04fb5eb96268cef7a0164eeed985f45116791dd
yao <yao.ntno@gmail.com>
2022-12-12 16:24:15 +0000

    add

--- rbcd/cli.py
+++ rbcd/cli.py
@@ -69,7 +69,7 @@ def validate(
 
 @click.command()
 @click.option("-c", "--config-path")
-@click.option("-l", "--limit", type=int, default=20000)
+@click.option("-l", "--limit", type=int, default=10000)
 @click.option("-t", "--n-trials", type=int, default=10)
 def search(
     config_path: str,
