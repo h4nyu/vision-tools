93b20533e3bf35b5b40b60694ea50428ff6e304f
yao <yao.ntno@gmail.com>
2022-12-17 13:18:25 +0000

    upload dataset

--- rbcd/dist.sh
+++ rbcd/dist.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p dist
-
-if [ ! -d "packages" ]; then
-    pip download -r requirements.txt -d packages
-fi
-if [ ! -f "dist/packages" ]; then
-    cp -r packages dist/packages
-fi
-
-cp requirements.txt dist/requirements.txt
-cp rbcd.py dist/rbcd.py
-cp -rf configs dist/configs
-cp -rf inference_models dist/inference_models
+zip -r packages.zip packages
+zip -r inference_models.zip inference_models
+zip -r configs.zip configs
