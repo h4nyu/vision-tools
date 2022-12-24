571cf418a02c59165bf4c93c14d7218ed79c618b
yao <yao.ntno@gmail.com>
2022-12-24 08:29:07 +0000

    fix code

--- rbcd/dist.sh
+++ rbcd/dist.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 rm *.zip
+if [ ! -d "packages" ]; then
+    pip download -r requirements.txt -d packages
+fi
 zip -r -j  packages.zip packages/*
 zip -r -j  models.zip models/*
 zip -r -j  configs.zip configs/*
